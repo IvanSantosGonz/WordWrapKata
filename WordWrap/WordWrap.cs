91759286ac188c2461fc38fd7661c162fb65ca6f
Ivan Santos <ivan.santos@leanmind.es>
2020-04-11 11:55:52 +0100

    RED: some_columns_without_spaces

---
+++ WordWrap/WordWrap.cs
@@ -0,0 +1,18 @@
+﻿namespace WordWrap
+{
+    public class WordWrap
+    {
+        public string Wrap(string text, int columns)
+        {
+            if (text.Length <= columns)
+            {
+                return text;
+            }
+
+            var recucedText = text.Substring(0, columns) + "\n";
+            var restOfTheText = text.Substring(columns);
+
+            return recucedText + restOfTheText;
+        }
+    }
+}
---
+++ WordWrapTests/WordWrapShould.cs
@@ -0,0 +1,59 @@
+using NUnit.Framework;
+
+/*("test", 7) -> "test"
+("hello world", 7) -> "hello--world"
+("a lot of words for a single line", 10) -> "a lot of--words for--a single--line"
+("this is a test", 4) -> "this--is a--test"
+("a longword", 6) -> "a long--word"
+("areallylongword", 6) -> "areall--ylongw--ord"*/
+
+namespace WordWrapTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+        
+        [Test]
+        public void text_is_empty()
+        {
+            var wordWrap = new WordWrap.WordWrap();
+            const string text = "test";
+            var result = wordWrap.Wrap("", 7);
+            
+            Assert.AreEqual("", result);
+        }
+
+        [Test]
+        public void columns_are_bigger_than_text()
+        {
+            var word = new WordWrap.WordWrap();
+            const string text = "test";
+            var result = word.Wrap(text, 7);
+            
+            Assert.AreEqual("test", result);
+        }
+        
+        [Test]
+        public void columns_are_shorter_than_text()
+        {
+            var word = new WordWrap.WordWrap();
+            const string text = "hello";
+            var result = word.Wrap(text, 3);
+            
+            Assert.AreEqual("hel\nlo", result);
+        }
+        
+        [Test]
+        public void some_columns_without_spaces()
+        {
+            var word = new WordWrap.WordWrap();
+            const string text = "hello";
+            var result = word.Wrap(text, 2);
+            
+            Assert.AreEqual("he\nll\no", result);
+        }
+    }
+}
