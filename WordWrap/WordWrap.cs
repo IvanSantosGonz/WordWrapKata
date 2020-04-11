4c1d991796cf64e3cb78acdb031bd51b367fa753
Ivan Santos <ivan.santos@leanmind.es>
2020-04-11 12:30:48 +0100

    GREEN: some_columns_with_spaces

--- WordWrap/WordWrap.cs
+++ WordWrap/WordWrap.cs
@@ -10,8 +10,20 @@
                 return reducedText + text;
             }
 
-            reducedText += text.Substring(0, columns) + "\n";
-            text = text.Substring(columns);
+            var wrappedWord = text.Substring(0, columns) + "\n";
+            var lastSpacePosition = wrappedWord.LastIndexOf(' ');
+
+            if (lastSpacePosition == -1)
+            {
+                reducedText += text.Substring(0, columns) + "\n";
+                text = text.Substring(columns);
+            }
+            else
+            {
+                reducedText += text.Substring(0, lastSpacePosition) + "\n";
+                text = text.Substring(lastSpacePosition + 1);
+            }
+
 
             return reducedText + Wrap(text, columns);
         }
